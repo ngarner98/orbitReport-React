e822a1960e1257ae7f11811637b65d790e0f6f14
ngarner98 <exmachina091098@gmail.com>
2023-11-18 23:28:25 -0600

    Graded assignment 6

---
+++ src/components/Banner.jsx
@@ -0,0 +1,14 @@
+export default function Banner(){
+
+    return(
+    <div>
+        <header>
+            Orbit Report
+        </header>
+        <p>
+            Use the buttons to see the satellites in that orbit group.
+        </p>
+    </div>
+    )
+}
+
--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,10 +1,31 @@
+import satData from "./satData";
+import "./styling.css"
+
 const Buttons = ({ filterByType, setSat, displaySats }) => {
+
   return (
-    <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
-    </div>
-  );
-};
+    <div className="flex-container">
+      <button>{displaySats.map((sat, id) => {
+       return (
+      <button onClick={() => filterByType(sat)} key={id}>
+      {sat} Orbit
+      </button>
+      );
+      })}
+      </button>
+    <button onClick= {() => setSat(satData)}>All Orbits</button>
+  </div>
+  )
+}
 
 export default Buttons;
+
+// return (
+//   <div>
+//     <button onClick={() => filterByType(sat)} key={id}>
+//       {sat} Orbit</button>
+    
+//   </div>
+//   );
+// });
+// <button onClick= {() => setSat(satData)}>All Orbits</button>
